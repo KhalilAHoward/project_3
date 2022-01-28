c90e17b70cc4ffcd48a0e26b6aec6def0453fe7e
Karissa DeLaunay <karissadelaunay@karissas-air.lan>
2022-01-28 15:38:11 -0800

    checking if we all can work seperatly at the same time

--- stayconnected/main_app/views.py
+++ stayconnected/main_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+#hi this is working while we are all in different folders
