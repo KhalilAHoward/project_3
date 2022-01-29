38085ca07d7b9d2807c0677a19f117b07271ba2a
Karissa DeLaunay <karissadelaunay@karissas-air.lan>
2022-01-28 22:01:23 -0800

    some starter code for us to work off of in the urls and views

--- stayconnected/main_app/urls.py
+++ stayconnected/main_app/urls.py
@@ -2,12 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('projects/', views.project_index, name='project_index'),
-    path('jobs/', views.job_index, name='job_index'),
+    path('', views.home, name='home'), #home page path
+    path('about/', views.about, name='about'), #page with info about the team and the app
+    #path('profile/', views.profile_index, name='index'), <----will add in later
+    #path('profile/<int:profile_id>/', views.profile_detail, name='detail'), <---- will add in later
+    path('jobs/', views.JobList.as_view(), name='job_index'), #this will be the page that all the job postings show on
+    path('projects/', views.ProjectList.as_view(), name='project_index'), #this will be the path for the page that all the projects show on
+    #path('profile/<int:profile_id>/assoc_project/<int:project_id>/', views.assoc_project, name='assoc_project'), <----- will add in later
+    #path('profile/<int:profile_id>/assoc_job/<int:job_id>/', views.assoc_job, name='assoc_job'), <------- will add in later
+    path('jobs/<int:pk>/', views.JobDetail.as_view(), name='jobs_detail'), #this will be the page when you click the job it shows the details
+    path('jobs/create/', views.JobCreate.as_view(), name='jobs_create'), #this will be the page you can have a form to create a job posting
+    path('projects/<int:pk>/', views.ProjectDetail.as_view(), name='projects_detail'), #page with details of the project when clicked on
+    path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'), #page to with a form to create the project
+    path('jobs/<int:pk>/update/', views.JobUpdate.as_view(), name='jobs_update'), #this will be the edit button on the details page of jobs to edit your own job posts
+    path('jobs/<int:pk>/delete/', views.JobDelete.as_view(), name='jobs_delete'), #this will be the delete button on the details page of jobs to delete your job posts
+    path('projects/<int:pk>/update/', views.ProjectUpdate.as_view(), name='projects_update'), #this will be the edit button on the details page of the project to edit your projects
+    path('projects/<int:pk>/delete/', views.ProjectDelete.as_view(), name='projects_delete'), #this will be the delete button on details page of the project to delete your own projects
 
 ]
-
+# commented out paths are because we dont have a model for it yet.  just creating basic paths for when we include a user profile -Karissa
 # charles 
 
--- stayconnected/main_app/views.py
+++ stayconnected/main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Project, Job
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
 
 # Add the following import
 from django.http import HttpResponse
@@ -10,12 +14,52 @@ def home(request):
 
 def about(request):
     return render(request, 'home.html')
+# changed the below functions to class based views but am not deleting incase we change it back
+# def project_index(request):
+#     return render(request, 'projects/project_index.html')
 
-def project_index(request):
-    return render(request, 'projects/project_index.html')
+# def job_index(request):
+#     return render(request, 'jobs/job_index.html')  
 
-def job_index(request):
-    return render(request, 'jobs/job_index.html')
+class ProjectList(ListView):
+    model = Project
+
+class JobList(ListView):
+    model = Job
+
+class JobDetail(DetailView):
+    model = Job
+
+
+class JobCreate(CreateView):
+    model = Job
+    fields = '__all__'
+
+class ProjectDetail(DetailView):
+    model = Project
+
+
+class ProjectCreate(CreateView):
+    model = Project
+    fields = '__all__'
+
+class JobUpdate(UpdateView):
+    model = Job
+    fields = '__all__'
+
+
+class JobDelete(DeleteView):
+    model = Job
+    success_url = '/TBD/' # To Be Determined on the success URL
+
+class ProjectUpdate(UpdateView):
+    model = Project
+    fields = '__all__'
+
+
+class ProjectDelete(DeleteView):
+    model = Project
+    success_url = '/TBD/'  # To Be Determined on the success URL
 
 
 #hi this is working while we are all in different folders
