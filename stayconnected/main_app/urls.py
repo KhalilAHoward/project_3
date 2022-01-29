4dc949c92004a36fb59b2be9676817706c23370b
Karissa DeLaunay <karissadelaunay@karissas-air.lan>
2022-01-28 16:47:11 -0800

    some urls and views

--- stayconnected/main_app/urls.py
+++ stayconnected/main_app/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('projects/', views.project_index, name='project_index'),
+    path('jobs/', views.job_index, name='job_index'),
+
 ]
 
 # charles 
--- stayconnected/main_app/views.py
+++ stayconnected/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Project, Job
 
 # Add the following import
 from django.http import HttpResponse
@@ -6,6 +7,15 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return render(request, 'home.html')
+
+def project_index(request):
+    return render(request, 'projects/project_index.html')
+
+def job_index(request):
+    return render(request, 'jobs/job_index.html')
 
 
 #hi this is working while we are all in different folders
