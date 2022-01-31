afd19d326f7a019be7b3aa3c605a38bf63c8fa3d
Karissa DeLaunay <karissadelaunay@karissas-air.lan>
2022-01-30 16:28:17 -0800

    made a projects form

--- stayconnected/main_app/models.py
+++ stayconnected/main_app/models.py
@@ -12,6 +12,12 @@ class Project(models.Model):
     description = models.TextField(max_length=250)
     link = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('project_detail', kwargs={'pk': self.id})
+
 
 class Job(models.Model):
     company_name = models.CharField(max_length=50)
