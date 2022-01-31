8ef131505b4c1d2ad5c03be2856079c77fceb8b9
Karissa DeLaunay <karissadelaunay@Karissas-MacBook-Air.local>
2022-01-31 15:57:35 -0800

    profile automatically created

--- stayconnected/main_app/models.py
+++ stayconnected/main_app/models.py
@@ -13,6 +13,7 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     link = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
