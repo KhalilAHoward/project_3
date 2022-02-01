9bdeb1ee48180007b6aabc2b9f545f54b5a38d4c
Karissa DeLaunay <karissadelaunay@karissas-air.lan>
2022-02-01 10:24:20 -0800

    new model everyone migrate

--- stayconnected/main_app/models.py
+++ stayconnected/main_app/models.py
@@ -84,3 +84,10 @@ class Comment(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for user_id: {self.user_id} @{self.url}"
+
