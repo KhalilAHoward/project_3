bc4345cb8d29844f2fe69124b416f037b2def5a9
Your Name <charlesjohnstoncpa@gmail.com>
2022-02-01 11:27:32 -0700

    photos

--- stayconnected/main_app/views.py
+++ stayconnected/main_app/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
-from .models import Project, Job, Profile
+from .models import Project, Job, Profile, Photo
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-
+import uuid
+import boto3
 
 # Add the following import
 from django.http import HttpResponse
+
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'sei-stay-connected'
 
 # Define the home view
 
@@ -134,3 +138,21 @@ def assoc_job(request, profile_id, job_id):
 
 
 # hi this is working while we are all in different folders
+
+def add_photo(request, user_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            Photo.objects.create(url=url, user_id=user_id)
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', user_id=user_id)
