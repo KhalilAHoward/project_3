61ea33bab8009e3382f2b0f6344f352383148e55
Karissa DeLaunay <karissadelaunay@karissas-air.lan>
2022-02-01 12:05:33 -0800

    everybody migrate

--- stayconnected/main_app/models.py
+++ stayconnected/main_app/models.py
@@ -66,9 +66,8 @@ class Comment(models.Model):
 
 class Photo(models.Model):
     url = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Photo for user_id: {self.user_id} @{self.url}"
+        return f"Photo for: {self} @{self.url}"
 
 
